d0cd7cc18249dc312c35ad9283fed7476d970130
James N. V. Cash <james.nvc@gmail.com>
2018-05-05 21:34:29 -0400

    Start stubbing out interface
    Thinking about how I'd like to be able to write "reactive" templates...

--- server.pl
+++ server.pl
@@ -8,81 +8,77 @@
                                            http_session_retractall/1,
                                            http_session_data/1,
                                            http_set_session_options/1]).
-:- use_module(library(http/html_write), [reply_html_page/2,
+:- use_module(library(http/html_write), [html_post//2, html_receive//1, reply_html_page/2,
                                          html//1]).
 :- use_module(library(http/http_parameters), [http_parameters/3]).
+:- use_module(library(css_write), [css//1, write_css/2]).
+:- use_module(library(list_util), [replicate/3]).
 
-% TODO:
-authenticate_user(_, _, _).
-register_user(_, _, _).
 
 go(Port) :-
     http_set_session_options([]),
     http_server(http_dispatch, [port(Port)]).
 
 % Routes
-:- http_handler(/, logged_in_page(home_page), []).
-:- http_handler('/login', login_handler, []).
-:- http_handler('/logout', logout_handler, []).
+:- http_handler(/, meal_plan_handler, []).
 
-% Login/out/register handlers
-logged_in(User) :-
-    http_session_data(logged_in(User)).
+% main handler
+meal_plan_handler(Request) :-
+    memberchk(method(get), Request),
+    reply_html_page(
+        [title('Eating Plan'),
+         \html_receive(css)],
+        \meal_plan_page
+    ).
 
-logged_in_page(Page, Request) :-
-    ( logged_in(_)
-    -> call(Page, Request)
-    ;  reply_html_page(
-           [title('Login or Register')],
-           \login_body)).
+include_css(CssDcg) -->
+    { write_css(CssDcg, CssTxt) },
+    html_post(css, style([], CssTxt)).
 
-login_body -->
-    html(div(id(body),
-             [div(id(login),
-                  [h1('Log In'),
-                   form([action('/login'), method('POST')],
-                        [label(['User Name:',
-                                input([type(text), name(uname)], [])]),
-                         label(['Password:',
-                                input([type(password), name(pw)])]),
-                         input([type(submit), value(submit)])]) ]),
-              div(id(register),
-                  [h1('Register'),
-                   form([action('/register'), method('POST')],
-                        [label('User Name:',
-                               input([type(text), name(uname)], [])),
-                         label(['Password',
-                                input([type(password), name(pw)])]),
-                         label(['Confirm Password',
-                                input([type(password), name(pw_conf)])]),
-                         input([type(submit), value(submit)])])])])).
+meal_plan_page -->
+    % get meals for user
+    { get_time(NowTs),
+      Today is integer(NowTs) div (3600 * 24),
+      End is Today + 7,
+      State = _{start_date: Today,
+                end_date: End,
+                meals_per_day: 2,
+                meals: [] } },
+    html([div(id(main),
+              [div(class('parameters'),
+                   % TODO: figure out how to make these vars work for "reactive"
+                   % maybe have pengines re-evaluate the DCG?
+                   [label(["Start Date",
+                           input([type(date), value(State.start_date)], [])]),
+                    label(["End Date",
+                           input([type(date), value(State.end_date)], [])]),
+                   label(["Meals per day",
+                          input([type(number), value(State.meals_per_day)], [])])
+                  ]),
+               div(class(meals),
+                   ul(\meal_items(State.meals))),
+               \calendar_page(State)])]).
 
+meal_items([]) --> [].
+meal_items([Meal|Rest]) -->
+    html(li(class(meal), Meal.name)), meal_items(Rest).
 
-login_handler(Request) :-
-    memberchk(method(post), Request),
-    http_parameters(Request, [], [form_data(Data)]),
-    memberchk(uname=Uname, Data),
-    memberchk(pw=Passwd, Data),
-    authenticate_user(Uname, Passwd, UserId),
-    http_session_asserta(logged_in(UserId)),
-    !, http_redirect(moved, '/', Request).
-login_handler(_Request) :-
-    % TODO: "flash" in session
-    reply_html_page([title('Login or Register')],
-                   \login_body).
+calendar_page(State) -->
+    html([\include_css(
+              css(['.calendar'([display(flex),
+                                'flex-direction'(row)],
+                               '.day'([margin('0.5em')],
+                                   '.meal-slot'([width('2em'),
+                                                 height('2em'),
+                                                 margin('0.5em'),
+                                                 'background-color'(green)])))])),
+          div(class(calendar),
+              \calendar_items(State.meals_per_day, State.start_date, State.end_date))]).
 
-logout_handler(Request) :-
-    memberchk(method(post), Request),
-    http_session_retractall(logged_in(_)),
-    !, http_redirect(moved, '/', Request).
-
-register_handler(Request) :-
-    memberchk(method(post), Request),
-    http_parameters(Request, [], [form_data(Data)]),
-    memberchk(uname=Uname, Data),
-    memberchk(pw=Passwd, Data),
-    memberchk(pw_conf=PasswdConf, Data),
-    Passwd = PasswdConf,
-    register_user(Uname, Passwd, UserId),
-    http_session_asserta(logged_in(UserId)),
-    !, http_redirect(moved, '/', Request).
+calendar_items(_, D, D) --> [].
+calendar_items(NSlots, S, E) -->
+    { Next is S + 1,
+      replicate(NSlots, div(class('meal-slot'), []), Slots) },
+    html(div(class(day),
+             [span(S)|Slots])),
+    calendar_items(NSlots, Next, E).
