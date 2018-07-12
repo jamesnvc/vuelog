85a612a0f9992d7935100977e895bba2f40dc685
James N. V. Cash <james.nvc@gmail.com>
2018-07-11 22:22:02 -0400

    Add pldoc to util

--- util.pl
+++ util.pl
@@ -1,6 +1,11 @@
 :- module(util, [ts_day/2,
                 listof//2]).
 
+%! ts_day(+Ts:timestamp, -Day:string) is det.
+%! ts_day(-Ts:timestamp, +Day:string) is semidet.
+%
+%  True when =Day= is a string representation of the day that =Ts= is
+%  on, in the format YYYY-MM-DD.
 ts_day(Ts, Day) :-
     number(Ts), !,
     format_time(string(Day), "%Y-%m-%d", Ts).
@@ -10,7 +15,7 @@ ts_day(Ts, Day) :-
 
 
 :- meta_predicate listof(3, +, ?, ?).
-%! listof(:DCG, +Elements:list)//.
+%! listof(:DCG, +Elements:list)//
 %
 %  Generates a list of elements from =DCG= (which presumably uses
 %  =html//1=) to make them work with Quench Vue.
