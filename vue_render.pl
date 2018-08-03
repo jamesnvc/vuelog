2d5507a1609cd1beaf19249307628ce832b50d3b
James N. V. Cash <james.nvc@gmail.com>
2018-08-02 21:36:24 -0400

    WIP: figuring out syntax

--- vue_render.pl
+++ vue_render.pl
@@ -11,45 +11,37 @@ meal_plan_page(State) -->
                 div([id(app)],
                     [div(class('parameters'),
                          [label(["Start Date",
-                                 input([type(date), 'v-model'(start_day),
-                                        value(State.start_day)], [])]),
+                                 vue_input([type(date), 'model'(start_day)], [])]),
                           label(["End Date",
-                                 input([type(date), 'v-model'(end_day),
-                                        value(State.end_day)], [])]),
+                                 vue_input([type(date), model(end_day)], [])]),
                           label(["Meals per day",
-                                 input([type(number), 'v-model.number'(meals_per_day),
-                                        value(State.meals_per_day)], [])])]),
+                                 vue_input([type(number), 'model.number'(meals_per_day)], [])])]),
                      div(class(meals), \meals),
-                     div(class(schedule), [h2("Schedule"),
-                                           button('@click.prevent'(regenSchedule),
-                                                  "New Schedule"),
-                                           \calendar(State)])])).
+                     div(class(schedule),
+                         [h2("Schedule"),
+                          vue_button(click(regenSchedule), "New Schedule"),
+                          \calendar(State)])])).
 
 meals -->
     vue_html([h2("Menu Options"),
-              vue_list(ul, [meals],
+              vue_list(ul, [meal in meals],
                        li([class(meal)]),
-                       [$name,
+                       [$meal.name,
                         br([]),
-                        p(['Makes a meal for ', $days, ' days']),
+                        p(['Makes a meal for ', $meal.days, ' days']),
                         br([]),
-                        vue_list(div, $tags,
+                        vue_list(div, [tag in $meal.tags],
                                  span,
-                                 $)]),
+                                 $tag)]),
               \add_meal]).
 
 
-meal_tag(Tag) -->
-    html(span('v-for'("tag in meal.tags"),
-              [span('v-text'(tag), Tag),
-               &(nbsp)])).
-
 add_meal -->
-    html(form(['@submit.prevent'("addMeal")],
-              [input([type(text), name(name), required(true),
-                      placeholder('Food name')]),
-               input([type(text), name(tags), required(true),
-                      placeholder('Comma-separated tags')]),
-               input([type(number), name(days), required(true), min(1),
-                      placeholder('How many days will this last?')]),
-               input([type(submit), value('Add')])])).
+    vue_html(vue_form([submit(addMeal)],
+                      [input([type(text), name(name), required(true),
+                              placeholder('Food name')]),
+                       input([type(text), name(tags), required(true),
+                              placeholder('Comma-separated tags')]),
+                       input([type(number), name(days), required(true), min(1),
+                              placeholder('How many days will this last?')]),
+                       input([type(submit), value('Add')])])).
