657b51aa3bdbf171dffea4df3510f1141d405edc
James N. V. Cash <james.nvc@gmail.com>
2018-08-04 21:52:13 -0400

    Writing down more syntax use ideas

--- vue_render.pl
+++ vue_render.pl
@@ -3,19 +3,27 @@
 */
 :- module(render, [meal_plan_page//1]).
 
+:- use_module(library(http/html_write), [html_post//2]).
 :- use_module(vue_html, [vue_context//2, vue_html//1]).
 :- use_module(library(css_write), [css//1, write_css/2]).
+
+include_css(CssDcg) -->
+    { write_css(CssDcg, CssTxt) },
+    html_post(css, style([], CssTxt)).
 
 meal_plan_page(State) -->
     vue_context(State,
                 div([id(app)],
                     [div(class('parameters'),
                          [label(["Start Date",
-                                 vue_input([type(date), 'model'(start_day)], [])]),
+                                 vue_input([type(date), model(start_day)], [])]),
                           label(["End Date",
                                  vue_input([type(date), model(end_day)], [])]),
                           label(["Meals per day",
-                                 vue_input([type(number), 'model.number'(meals_per_day)], [])])]),
+                                 % XXX: some way to indicate that
+                                 % meals_per_day should be treated as
+                                 % a number?
+                                 vue_input([type(number), model(meals_per_day)], [])])]),
                      div(class(meals), \meals),
                      div(class(schedule),
                          [h2("Schedule"),
@@ -24,15 +32,16 @@ meal_plan_page(State) -->
 
 meals -->
     vue_html([h2("Menu Options"),
-              vue_list(ul, [meal in meals],
-                       li([class(meal)]),
-                       [$meal.name,
-                        br([]),
-                        p(['Makes a meal for ', $meal.days, ' days']),
-                        br([]),
-                        vue_list(div, [tag in $meal.tags],
-                                 span,
-                                 $tag)]),
+              ul(vue_list(
+                     [meal in meals],
+                     li([class(meal)]),
+                     [$meal.name,
+                      br([]),
+                      p(['Makes a meal for ', $meal.days, ' days']),
+                      br([]),
+                      vue_list([tag in $meal.tags],
+                               span,
+                               $tag)])),
               \add_meal]).
 
 
@@ -45,3 +54,26 @@ add_meal -->
                        input([type(number), name(days), required(true), min(1),
                               placeholder('How many days will this last?')]),
                        input([type(submit), value('Add')])])).
+
+calendar_css -->
+    css(['.calendar'(
+             [display(flex), 'flex-direction'(row)],
+             '.day'([margin('0.5em')],
+                    '.meal-slot'(['min-height'('2em'),
+                                  margin('0.5em'),
+                                  'text-align'(center),
+                                  'background-color'(darkgreen),
+                                  color(white)])))]).
+
+calendar -->
+    vue_html([\include_css(calendar_css),
+              div(class(calendar),
+                  vue_list([slot in slots],
+                           div(class(day)),
+                           [$slot.day, \slots($slot.entries)]))
+             ]).
+
+slots(Entries) -->
+    vue_html(vue_list([entry in Entries],
+                      div(class('meal-slot')),
+                      $entry.name)).
